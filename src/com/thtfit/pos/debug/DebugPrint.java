ef29a3ae5c1bb4844388705352a8435371cfa5f4
abner <abner@thtfit.com>
2015-09-29 17:00:31 +0800

    optimizes debug print.

--- src/com/thtfit/pos/debug/DebugPrint.java
+++ src/com/thtfit/pos/debug/DebugPrint.java
@@ -1,22 +1,25 @@
 package com.thtfit.pos.debug;
 
+import com.thtfit.pos.R.bool;
+
 import android.util.Log;
 
 public class DebugPrint
 {
 	private String TAG = null;
-	
+
 	private static final boolean DEBUG = true;
-	
+	private static boolean isDebug = false;
+
 	public DebugPrint(boolean isDebug, String TAG)
 	{
-		//DEBUG = isDebug;
+		this.isDebug = isDebug;
 		this.TAG = TAG;
 	}
 
 	public void D(String msg)
 	{
-		if (DEBUG)
+		if (isDebug && DEBUG)
 		{
 			Log.d(TAG, msg);
 		}
@@ -24,7 +27,7 @@ public class DebugPrint
 
 	public void E(String msg)
 	{
-		if (DEBUG)
+		if (isDebug && DEBUG)
 		{
 			Log.e(TAG, msg);
 		}
@@ -32,7 +35,7 @@ public class DebugPrint
 
 	public void V(String msg)
 	{
-		if (DEBUG)
+		if (isDebug && DEBUG)
 		{
 			Log.v(TAG, msg);
 		}
@@ -40,56 +43,53 @@ public class DebugPrint
 
 	public void W(String msg)
 	{
-		if (DEBUG)
+		if (isDebug && DEBUG)
 		{
 			Log.w(TAG, msg);
 		}
 	}
+
 	public void I(String msg)
 	{
-		if (DEBUG)
+		if (isDebug && DEBUG)
 		{
 			Log.i(TAG, msg);
 		}
 	}
-	
-	
-	
-	
-	
-	public static void d(String tag,String msg)
+
+	public static void d(String tag, String msg)
 	{
 		if (DEBUG)
 		{
 			Log.d(tag, msg);
 		}
 	}
-	
-	public static void e(String tag,String msg)
+
+	public static void e(String tag, String msg)
 	{
 		if (DEBUG)
 		{
 			Log.e(tag, msg);
 		}
 	}
-	
-	public static void v(String tag,String msg)
+
+	public static void v(String tag, String msg)
 	{
 		if (DEBUG)
 		{
 			Log.v(tag, msg);
 		}
 	}
-	
-	public static void w(String tag,String msg)
+
+	public static void w(String tag, String msg)
 	{
 		if (DEBUG)
 		{
 			Log.w(tag, msg);
 		}
 	}
-	
-	public static void i(String tag,String msg)
+
+	public static void i(String tag, String msg)
 	{
 		if (DEBUG)
 		{
